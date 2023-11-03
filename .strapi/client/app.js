619e265915e06dd2f36305da51180c90f4a8de2a
Emanuel Saramago <emanuel.reg@outlook.com>
2023-11-03 15:11:40 +0000

    remove cloud

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -5,9 +5,8 @@
 import contentTypeBuilder from "@strapi/plugin-content-type-builder/strapi-admin";
 import email from "@strapi/plugin-email/strapi-admin";
 import upload from "@strapi/plugin-upload/strapi-admin";
-import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
 import i18N from "@strapi/plugin-i18n/strapi-admin";
-import strapiCloud from "@strapi/plugin-cloud/strapi-admin";
+import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
 import { renderAdmin } from "@strapi/admin/strapi-admin";
 
 renderAdmin(document.getElementById("strapi"), {
@@ -15,8 +14,7 @@ renderAdmin(document.getElementById("strapi"), {
     "content-type-builder": contentTypeBuilder,
     email: email,
     upload: upload,
-    "users-permissions": usersPermissions,
     i18n: i18N,
-    "strapi-cloud": strapiCloud,
+    "users-permissions": usersPermissions,
   },
 });
